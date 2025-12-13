3df473abd5572b1b496f772adb819470bd6a398a
chandraputra06 <chandra.wisesa06@gmail.com>
2025-12-13 14:32:34 +0800

    Initial commit: Set up project structure and base configuration

---
+++ app/Http/Controllers/StudyProgramController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StudyProgram;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreStudyProgramRequest;
+use App\Http\Requests\UpdateStudyProgramRequest;
+
+class StudyProgramController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudyProgramRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudyProgram $studyProgram)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudyProgram $studyProgram)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudyProgramRequest $request, StudyProgram $studyProgram)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudyProgram $studyProgram)
+    {
+        //
+    }
+}
